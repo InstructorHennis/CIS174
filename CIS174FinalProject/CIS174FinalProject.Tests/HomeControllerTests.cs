3cd8143b5b10debd3fd885fbdb0f66db8d1e0023
Evan Hennis <evanhennis@gmail.com>
2026-01-19 12:47:46 -0600

    Fixed unit tests

--- CIS174FinalProject/CIS174FinalProject.Tests/HomeControllerTests.cs
+++ CIS174FinalProject/CIS174FinalProject.Tests/HomeControllerTests.cs
@@ -1,5 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using CIS174FinalProject.Controllers;
+using CIS174FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CIS174FinalProject.Tests;
 
@@ -10,7 +12,13 @@ public class HomeControllerTests
     public void Index_ReturnsViewResult()
     {
         // Arrange
-        var controller = new HomeController();
+        var options = new DbContextOptionsBuilder<LibraryContext>()
+            .UseInMemoryDatabase(databaseName: "TestLibrary")
+            .Options;
+
+        // Act
+        using var context = new LibraryContext(options);
+        var controller = new HomeController(context);
 
         // Act
         var result = controller.Index();
